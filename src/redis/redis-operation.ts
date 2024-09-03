6a62ead33d0dc437dfc4cd41f998ea448c7eeac8
asadsanto10 <asad.santo.20@gmail.com>
2024-09-03 23:24:12 +0600

    redis

--- src/redis/redis-operation.ts
+++ src/redis/redis-operation.ts
@@ -2,8 +2,14 @@ import redis from './initRedis';
 
 export const redisOperations = async () => {
 	try {
-		redis.set('test', 'asadas');
-		const d = await redis.get('test');
+		redis.set('bike:1', 'bike');
+		redis.mset(['lang:1', 'js', 'lang:2', 'react']);
+		redis.set('count', 0);
+		redis.incr('count');
+		redis.incr('count');
+		redis.incrby('count', 10);
+		redis.expire('count', 10);
+		const d = await redis.mget(['bike:1', 'lang:1', 'count']);
 		console.log({ d });
 	} catch (error) {
 		console.log(error);
